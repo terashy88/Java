cae5481de1a1aa1e2dd7126e3ee7e19ec0d22d6c
terashy <terashy@bluewin.ch>
2022-10-03 12:58:44 +0200

    first commit

---
+++ Math.java
@@ -0,0 +1,18 @@
+/**
+ * testNew
+ */
+public class Math {
+
+    public static void main(String[] args) {
+
+        int number1 = 3;
+        int number2 = 6;
+
+        System.out.println(number1 + number2); // Plus
+        System.out.println(number1 - number2); // Minus
+        System.out.println(number1 * number2); // Multiplication
+        System.out.println(number1 / number2); // Division
+        System.out.println(number2 % number1); // Modular multiplication
+        System.out.println(number2 % number1 + 5); // Modular multiplication
+    }
+}
